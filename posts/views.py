641444880959a9b5056efbb3e095a606f2654b66
David Sánchez <d4vsanchez@gmail.com>
2020-03-29 12:43:24 -0500

    Bring data of posts from the feed

--- posts/views.py
+++ posts/views.py
@@ -1,8 +1,5 @@
 """Posts views"""
 
-# Utilities
-from datetime import datetime
-
 # Django
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
@@ -10,39 +7,14 @@ from django.contrib.auth.decorators import login_required
 # Forms
 from posts.forms import PostForm
 
+# Models
+from posts.models import Post
+
 
 @login_required
 def list_posts(request):
     """List existing posts"""
-    posts = [
-        {
-            "title": "Mont Blanc",
-            "user": {
-                "name": "Yesica Cortes",
-                "picture": "https://picsum.photos/60/60/?image=1027",
-            },
-            "timestamp": datetime.now(),
-            "photo": "https://i.picsum.photos/id/658/600/600.jpg",
-        },
-        {
-            "title": "Via Lactea",
-            "user": {
-                "name": "C. Vander",
-                "picture": "https://picsum.photos/60/60/?image=1005",
-            },
-            "timestamp": datetime.now(),
-            "photo": "https://i.picsum.photos/id/656/600/600.jpg",
-        },
-        {
-            "title": "Nuevo auditorio",
-            "user": {
-                "name": "Thespianartist",
-                "picture": "https://picsum.photos/60/60/?image=883",
-            },
-            "timestamp": datetime.now(),
-            "photo": "https://i.picsum.photos/id/657/600/600.jpg",
-        },
-    ]
+    posts = Post.objects.all().order_by("-created")
     return render(request, "posts/feed.html", {"posts": posts})
 
 
