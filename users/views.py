cc12f896aaf93de6be46f3168a459bc463b29445
David Sánchez <d4vsanchez@gmail.com>
2020-03-28 21:46:11 -0500

    Add the sign up behavior

--- users/views.py
+++ users/views.py
@@ -1,9 +1,14 @@
 """User views"""
 
 # Django
+from django.db.utils import IntegrityError
+from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
+
+# Models
+from users.models import Profile
 
 
 def login_view(request):
@@ -20,6 +25,35 @@ def login_view(request):
                 request, "users/login.html", {"error": "Wrong username or password"}
             )
     return render(request, "users/login.html")
+
+
+def signup_view(request):
+    """Signup view."""
+    if request.method == "POST":
+        username = request.POST["username"]
+        passwd = request.POST["passwd"]
+        passwd_confirmation = request.POST["passwd_confirmation"]
+
+        if passwd != passwd_confirmation:
+            return render(
+                request,
+                "users/signup.html",
+                {"error": "Password confirmation does not match"},
+            )
+
+        try:
+            user = User.objects.create_user(username=username, password=passwd)
+        except:
+            return render(
+                request, "users/signup.html", {"error": "Username already taken"}
+            )
+        user.first_name = request.POST["first_name"]
+        user.last_name = request.POST["last_name"]
+        user.email = request.POST["email"]
+
+        profile = Profile.objects.create(user=user)
+        return redirect("login")
+    return render(request, "users/signup.html")
 
 
 @login_required
