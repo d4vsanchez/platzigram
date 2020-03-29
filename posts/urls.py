49274186820b2dfad6958579eaeb00b94d493c55
David Sánchez <d4vsanchez@gmail.com>
2020-03-29 14:36:42 -0500

    Creating post detail view

--- posts/urls.py
+++ posts/urls.py
@@ -8,5 +8,6 @@ from posts import views
 
 urlpatterns = [
     path("", views.PostsFeedView.as_view(), name="feed"),
-    path("posts/new/", views.create_post, name="create"),
+    path("posts/<str:pk>", views.PostDetailView.as_view(), name="detail"),
+    path("posts/new", views.create_post, name="create"),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,8 +1,8 @@
 """Posts views"""
 
 # Django
-from django.views.generic import ListView
 from django.shortcuts import render, redirect
+from django.views.generic import ListView, DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.decorators import login_required
 
@@ -21,6 +21,14 @@ class PostsFeedView(LoginRequiredMixin, ListView):
     ordering = ("-created",)
     paginate_by = 2
     context_object_name = "posts"
+
+
+class PostDetailView(LoginRequiredMixin, DetailView):
+    """Return post detail."""
+
+    template_name = "posts/detail.html"
+    queryset = Post.objects.all()
+    context_object_name = "post"
 
 
 @login_required
