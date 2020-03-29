b1d0c9c995bbcde28108092be97a294caafe2d3d
David Sánchez <d4vsanchez@gmail.com>
2020-03-28 20:13:11 -0500

    Creating the Post model

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,28 @@
+# Django
 from django.contrib import admin
 
-# Register your models here.
+# Models
+from posts.models import Post
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    """Post admin."""
+
+    list_display = ('pk', 'title', 'photo', 'user')
+    list_display_links = ('pk', 'title')
+    search_fields = ('user__email', 'user__username', 'title')
+    list_filter = ('created', 'modified', 'user__is_active')
+
+    fieldsets = (
+        ('Photo information', {
+            'fields': (
+                ('title', 'photo')
+            ),
+        }),
+        ('Author information', {
+            'fields': (
+                ('user',)
+            ),
+        }),
+    )
