b780025e6ae20d9d492b37abb09dd7ccef1bdc4d
David Sánchez <d4vsanchez@gmail.com>
2020-03-28 20:44:01 -0500

    Refactoring templates

--- posts/views.py
+++ posts/views.py
@@ -6,35 +6,36 @@ from datetime import datetime
 # Django
 from django.shortcuts import render
 
+
 def list_posts(request):
     """List existing posts"""
     posts = [
         {
-            'title': 'Mont Blanc',
-            'user': {
-                'name': 'Yesica Cortes',
-                'picture': 'https://picsum.photos/60/60/?image=1027'
+            "title": "Mont Blanc",
+            "user": {
+                "name": "Yesica Cortes",
+                "picture": "https://picsum.photos/60/60/?image=1027",
             },
-            'timestamp': datetime.now(),
-            'photo': 'https://i.picsum.photos/id/658/200/200.jpg',
+            "timestamp": datetime.now(),
+            "photo": "https://i.picsum.photos/id/658/600/600.jpg",
         },
         {
-            'title': 'Via Lactea',
-            'user': {
-                'name': 'C. Vander',
-                'picture': 'https://picsum.photos/60/60/?image=1005'
+            "title": "Via Lactea",
+            "user": {
+                "name": "C. Vander",
+                "picture": "https://picsum.photos/60/60/?image=1005",
             },
-            'timestamp': datetime.now(),
-            'photo': 'https://i.picsum.photos/id/656/200/200.jpg',
+            "timestamp": datetime.now(),
+            "photo": "https://i.picsum.photos/id/656/600/600.jpg",
         },
         {
-            'title': 'Nuevo auditorio',
-            'user': {
-                'name': 'Thespianartist',
-                'picture': 'https://picsum.photos/60/60/?image=883'
+            "title": "Nuevo auditorio",
+            "user": {
+                "name": "Thespianartist",
+                "picture": "https://picsum.photos/60/60/?image=883",
             },
-            'timestamp': datetime.now(),
-            'photo': 'https://i.picsum.photos/id/657/200/200.jpg',
+            "timestamp": datetime.now(),
+            "photo": "https://i.picsum.photos/id/657/600/600.jpg",
         },
     ]
-    return render(request, 'feed.html', {'posts': posts})
+    return render(request, "posts/feed.html", {"posts": posts})
